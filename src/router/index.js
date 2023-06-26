1cfc808f5fcc145f2190274159fddac2a60329be
Madeline Caples <madelinecaples@Madelines-MacBook-Pro.local>
2023-06-26 10:36:33 -0400

    completing music app

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
 import About from '@/views/AboutView.vue'
 import Manage from '@/views/ManageView.vue'
+import Song from '@/views/SongView.vue'
 import useUserStore from '@/stores/user'
 
 const routes = [
@@ -33,6 +34,11 @@ const routes = [
     redirect: {
       name: 'manage'
     }
+  },
+  {
+    name: 'song',
+    path: '/song/:id',
+    component: Song
   },
   {
     path: '/:catchAll(.*)*',
